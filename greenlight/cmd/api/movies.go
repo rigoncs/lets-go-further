0bcce8f63f188d162eac25f4a624ee65a127997e
rigon <2412789408@qq.com>
2025-10-21 10:00:56 +0800

    feat: chap4.5 Validating JSON input

--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/rigoncs/lets-go-further/internal/data"
+	"github.com/rigoncs/lets-go-further/internal/validator"
 	"net/http"
 	"time"
 )
@@ -17,6 +18,18 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
