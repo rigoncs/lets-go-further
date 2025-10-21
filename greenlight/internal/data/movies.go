471c947aa029a918ce4b865796a6f4ae0c88b98f
rigon <2412789408@qq.com>
2025-10-21 20:30:25 +0800

    feat: chap7.2 Creating a new movie

--- greenlight/internal/data/movies.go
+++ greenlight/internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"github.com/lib/pq"
 	"github.com/rigoncs/lets-go-further/internal/validator"
 	"time"
 )
@@ -38,7 +39,12 @@ type MovieModel struct {
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	query := `
+        INSERT INTO movies (title, year, runtime, genres) 
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, created_at, version`
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
