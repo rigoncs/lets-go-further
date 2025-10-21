5b989e9437e4096b4e4379090d28f424ab0ee300
rigon <2412789408@qq.com>
2025-10-21 19:47:28 +0800

    chore: remove useless code

--- greenlight/migrations/000001_create_movies_table.up.sql
+++ greenlight/migrations/000001_create_movies_table.up.sql
@@ -1,6 +1,3 @@
-
-GRANt CREATE ON DATABASE greenlight TO greenlight;
-
 CREATE TABLE IF NOT EXISTS movies (
     id bigserial PRIMARY KEY,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
