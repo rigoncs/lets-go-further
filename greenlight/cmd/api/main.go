ba7c16b8ea5c2690d97929cb53b242fee49a233b
rigon <2412789408@qq.com>
2025-10-21 20:03:32 +0800

    feat: chap7.1 Setting up the movie model

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/rigoncs/lets-go-further/internal/data"
 	"log/slog"
 	"net/http"
 	"os"
@@ -29,6 +30,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -58,6 +60,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
