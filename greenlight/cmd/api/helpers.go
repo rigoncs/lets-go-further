158d69044741ef629f5e81061f4c8ef8995ae8a4
rigon <2412789408@qq.com>
2025-10-22 20:11:16 +0800

    feat: chap13.4 Sending background emails

--- greenlight/cmd/api/helpers.go
+++ greenlight/cmd/api/helpers.go
@@ -116,3 +116,16 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 	}
 	return i
 }
+
+func (app *application) background(fn func()) {
+	go func() {
+		defer func() {
+			pv := recover()
+			if pv != nil {
+				app.logger.Error(fmt.Sprintf("%s", pv))
+			}
+		}()
+
+		fn()
+	}()
+}
