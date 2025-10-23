cb4e4928f3fcd93289a4c6e9c923646704ff79be
rigon <2412789408@qq.com>
2025-10-23 10:01:16 +0800

    feat: chap15.3 Authenticating requests

--- greenlight/cmd/api/middleware.go
+++ greenlight/cmd/api/middleware.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/rigoncs/lets-go-further/internal/data"
+	"github.com/rigoncs/lets-go-further/internal/validator"
 	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 )
@@ -64,6 +68,45 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 			return
 		}
 		mu.Unlock()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization")
+		authorizationHeader := r.Header.Get("Authorization")
+		if authorizationHeader == "" {
+			r = app.contextSetUser(r, data.AnonymousUser)
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		headerParts := strings.Split(authorizationHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		token := headerParts[1]
+		v := validator.New()
+		if data.ValidateTokenPlaintext(v, token); !v.Valid() {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		user, err := app.models.Users.GetForToken(data.ScopeAuthentication, token)
+		if err != nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+				app.invalidAuthenticationTokenResponse(w, r)
+			default:
+				app.serverErrorResponse(w, r, err)
+			}
+			return
+		}
+
+		r = app.contextSetUser(r, user)
 		next.ServeHTTP(w, r)
 	})
 }
