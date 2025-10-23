f065178eaf6f3780faba2cab242dd5e3487f1d11
rigon <2412789408@qq.com>
2025-10-23 16:30:47 +0800

    feat: chap18.2 Creating custom metrics

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"github.com/rigoncs/lets-go-further/internal/data"
 	"github.com/rigoncs/lets-go-further/internal/mailer"
 	"log/slog"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -95,6 +97,20 @@ func main() {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+
+	expvar.NewString("version").Set(version)
+
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+
+	expvar.Publish("timestamp", expvar.Func(func() any {
+		return time.Now().Unix()
+	}))
 
 	app := &application{
 		config: cfg,
