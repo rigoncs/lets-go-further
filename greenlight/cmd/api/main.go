fd7f33f7d119f5bdaa701cb73a795ae0c9a316ee
rigon <2412789408@qq.com>
2025-10-23 15:37:06 +0800

    feat: chap17.3 Simple CORS requests

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/rigoncs/lets-go-further/internal/mailer"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -36,6 +37,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -67,6 +71,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "0ac2db9c291cbb", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "ae57506f821b8d", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.example.com>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
